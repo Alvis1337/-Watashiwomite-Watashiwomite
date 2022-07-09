2ef6205fff6e4883a3cb37abbf38cc9a2586fdcd
Chris Alvis <alvisc@hocking.edu>
2022-07-08 21:18:27 -0400

    We can get a token, get a code, get a user from mal, refresh the token, get mal watchlist, get sonarr system status, get series tvdbid. we are working on grab and add in postman. this will let us grab the series tvdbid and add it to sonarr and we will be free from this hell.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MalWatchlist;
+use App\Http\Controllers\Sonarr;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
@@ -21,7 +22,16 @@ Route::get('/', function () {
 });
 Route::get('/auth/oauth', [MalWatchlist::class, 'codeGrant']);
 Route::get('/auth/expired_tokens', [MalWatchlist::class, 'refreshToken']);
+Route::get('/sonarr/system', [Sonarr::class, 'getSystemStatus']);
+Route::get('/grab-and-add', [Sonarr::class, 'grabAndAdd']);
+
 Route::get('/user', function (Request $request) {
     $user_id = $request->user_id;
     return App::call('App\Http\Controllers\MalWatchlist@getWatchlist', ['user_id' => $user_id]);
+});
+
+//http://192.168.5.59:8989/api/series/lookup?term=The%20Blacklist&apikey=48e9ab7124c04a4092f803bdd78916f2
+Route::get('/series-lookup', function (Request $request) {
+    $anime = $request->anime;
+    return App::call('App\Http\Controllers\Sonarr@seriesLookup', ['anime' => $anime]);
 });
